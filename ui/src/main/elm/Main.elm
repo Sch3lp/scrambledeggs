d9e2acb3df90092553ba534bda3d3f2a16c2a8f8
Laurens <laurens.decock@odisee.be>
2020-12-16 02:48:32 +0100

    - Set up elm-ui - Explicitly import from HTML to avoid conflicts with names in Element.* - Update view function to return an elm-ui Layout - Updated the HTML.Input field with Element.Input.Text field - Add --debug flag to elm-live

--- ui/src/main/elm/Main.elm
+++ ui/src/main/elm/Main.elm
@@ -4,16 +4,22 @@ module Main exposing (..)
 
 This application is broken up into three key parts:
 
-  1. Model  - a full definition of the application's state
-  2. Update - a way to step the application state forward
-  3. View   - a way to visualize our application state with HTML
+1.  Model - a full definition of the application's state
+2.  Update - a way to step the application state forward
+3.  View - a way to visualize our application state with HTML
 
 This clean division of concerns is a core part of Elm. You can read more about
 this in <http://guide.elm-lang.org/architecture/index.html>
+
 -}
 
 import Browser
-import Html exposing (..)
+import Element exposing (Element, column, el, fill, layout, rgb, rgb255, rgba, row, text)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Attribute, Html, div, footer, h1, header, input, p, section)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.Lazy exposing (lazy)
@@ -26,7 +32,9 @@ main : Program (Maybe String) Model Msg
 main =
     Browser.document
         { init = init
-        , view = \model -> { title = "Scramble • Diabotical Ladder", body = [view model] }
+        , view =
+            \model ->
+                { title = "Scramble • Diabotical Ladder", body = [ view model ] }
         , update = update
         , subscriptions = subscriptions
         }
@@ -35,15 +43,20 @@ main =
 
 -- MODEL
 
+
 type ApiPost
     = Failure String
     | Loading
 
+
+
 -- The full application state of our todo app.
+
+
 type alias Model =
     { nicknameField : String
     , successMessage : String
-    , postRegisterPlayer: Maybe ApiPost
+    , postRegisterPlayer : Maybe ApiPost
     }
 
 
@@ -55,11 +68,11 @@ emptyModel =
     }
 
 
-init : (Maybe String) -> ( Model, Cmd Msg )
+init : Maybe String -> ( Model, Cmd Msg )
 init _ =
-  ( emptyModel
-  , Cmd.none
-  )
+    ( emptyModel
+    , Cmd.none
+    )
 
 
 
@@ -79,6 +92,8 @@ type Msg
 
 
 -- How we update our Model on a given Msg?
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
@@ -96,136 +111,195 @@ update msg model =
         GotRegisterPlayerResponse result ->
             handleRegisterPlayerResponse model result
 
+
+
 -- SUBSCRIPTIONS
+
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  Sub.none
+    Sub.none
+
 
 
 -- HTTP
 
-registerPlayer: Model -> ( Model, Cmd Msg)
+
+registerPlayer : Model -> ( Model, Cmd Msg )
 registerPlayer model =
     let
-        playerNameJson = newPlayerName model.nicknameField
-    in ( { model | postRegisterPlayer = Just Loading }
-      , Http.post
-          { url = "/api/register"
-          , body = Http.jsonBody playerNameJson
-          , expect = expectStringWithErrorHandling GotRegisterPlayerResponse
-          }
-      )
+        playerNameJson =
+            newPlayerName model.nicknameField
+    in
+    ( { model | postRegisterPlayer = Just Loading }
+    , Http.post
+        { url = "/api/register"
+        , body = Http.jsonBody playerNameJson
+        , expect = expectStringWithErrorHandling GotRegisterPlayerResponse
+        }
+    )
+
+
 
 -- This is a constructor function to get a PlayerNameJson object from a string
-newPlayerName: String -> Encode.Value
+
+
+newPlayerName : String -> Encode.Value
 newPlayerName name =
     Encode.object [ ( "name", Encode.string name ) ]
 
-handleRegisterPlayerResponse: Model -> Result ApiError value -> ( Model, Cmd Msg)
+
+handleRegisterPlayerResponse : Model -> Result ApiError value -> ( Model, Cmd Msg )
 handleRegisterPlayerResponse model result =
     case result of
         Ok _ ->
-          ( { model | nicknameField = ""
-            , successMessage = "Yay! You are now registered for the Scramble Ladder."
-            } , Cmd.none )
+            ( { model
+                | nicknameField = ""
+                , successMessage = "Yay! You are now registered for the Scramble Ladder."
+              }
+            , Cmd.none
+            )
+
         -- When it's a BadRequest, we care about the response, because it contains an insightful error message.
-        Err (BadRequest msg) -> ( { model | postRegisterPlayer = Just (Failure msg)}, Cmd.none )
+        Err (BadRequest msg) ->
+            ( { model | postRegisterPlayer = Just (Failure msg) }, Cmd.none )
+
         -- When it's any other ApiError we don't care about specifics.
-        _ -> ( { model | postRegisterPlayer = Just (Failure "Something went wrong.")}, Cmd.none )
+        _ ->
+            ( { model | postRegisterPlayer = Just (Failure "Something went wrong.") }, Cmd.none )
+
+
 
 -- When we need to decode, replace String with "a" and provide a Decoder function as 2nd argument to transform "a"
+
+
 expectStringWithErrorHandling : (Result ApiError String -> msg) -> Expect msg
 expectStringWithErrorHandling toMsg =
-  expectStringResponse toMsg <|
-    \response ->
-      case response of
-        Http.BadUrl_ url ->
-            Err (BadUrl url)
+    expectStringResponse toMsg <|
+        \response ->
+            case response of
+                Http.BadUrl_ url ->
+                    Err (BadUrl url)
 
-        Http.Timeout_ ->
-            Err Timeout
+                Http.Timeout_ ->
+                    Err Timeout
 
-        Http.NetworkError_ ->
-            Err NetworkError
+                Http.NetworkError_ ->
+                    Err NetworkError
 
-        Http.BadStatus_ metadata body ->
-            Err (BadRequest body)
+                Http.BadStatus_ metadata body ->
+                    Err (BadRequest body)
 
-        Http.GoodStatus_ metadata body ->
-            Ok body
+                Http.GoodStatus_ metadata body ->
+                    Ok body
 
-type ApiError = BadRequest String
-                | NetworkError
-                | Timeout
-                | BadUrl String
+
+type ApiError
+    = BadRequest String
+    | NetworkError
+    | Timeout
+    | BadUrl String
+
+
 
 -- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    div
-        [ class "todomvc-wrapper"
-        , style "visibility" "hidden"
-        ]
-        [ section
-            [ class "todoapp" ]
-            [ lazy viewInput model.nicknameField
-            , lazy viewRegisterButton model.successMessage
-            , lazy viewSuccessMessage model.successMessage
-            , lazy viewFailureMessage model.postRegisterPlayer
+    -- I don't understand why we have to pipe
+    layout
+        []
+    <|
+        column
+            [ Background.color (rgb255 92 99 118) ]
+            [ el [] (text "HI")
+            , viewInput model.nicknameField
             ]
-        , infoFooter
-        ]
 
+
+
+-- [ section
+--     [ class "todoapp" ]
+--     [ lazy viewInput model.nicknameField
+--     , lazy viewRegisterButton model.successMessage
+--     , lazy viewSuccessMessage model.successMessage
+--     , lazy viewFailureMessage model.postRegisterPlayer
+--     ]
+-- , infoFooter
+-- ]
+
+
+viewRegisterButton : String -> Html Msg
 viewRegisterButton message =
     let
         isDisabled =
-                    if String.isEmpty message then
-                        disabled False
-                    else
-                        disabled True
+            if String.isEmpty message then
+                disabled False
+
+            else
+                disabled True
     in
-        button [ isDisabled, onClick RegisterButtonClicked ] [ text "Register" ]
+    Html.button [ isDisabled, onClick RegisterButtonClicked ] [ Html.text "Register" ]
+
 
 viewSuccessMessage : String -> Html Msg
 viewSuccessMessage message =
     let
         cssVisibility =
-                    if String.isEmpty message then
-                        "hidden"
-                    else
-                        "visible"
+            if String.isEmpty message then
+                "hidden"
+
+            else
+                "visible"
     in
-        p [style "visibility" cssVisibility] [ text message ]
+    p [ style "visibility" cssVisibility ] [ Html.text message ]
+
 
 viewFailureMessage : Maybe ApiPost -> Html Msg
 viewFailureMessage apiPostResult =
     let
-        (cssVisibility, message) =
+        ( cssVisibility, message ) =
             case apiPostResult of
-                Just (Failure msg) -> ("visible", msg)
-                _ -> ("hidden", "")
-    in
-        p [style "visibility" cssVisibility] [ text message ]
+                Just (Failure msg) ->
+                    ( "visible", msg )
 
-viewInput : String -> Html Msg
-viewInput task =
-    header
-        [ class "header" ]
-        [ h1 [] [ text "Register for Scrambled!" ]
-        , input
-            [ class "new-todo"
-            , placeholder "Your nickname (you use in Diabotical)"
-            , autofocus True
-            , value task
-            , name "nickname"
-            , onInput UpdateNicknameField
-            , onEnter RegisterButtonClicked
-            ]
+                _ ->
+                    ( "hidden", "" )
+    in
+    p [ style "visibility" cssVisibility ] [ Html.text message ]
+
+
+viewInput : String -> Element Msg
+viewInput value =
+    column
+        []
+        [ el [] (text "Register for Scrambled!")
+        , Input.text
             []
+            { label = Input.labelLeft [] (text "Username")
+            , onChange = UpdateNicknameField
+            , placeholder = Just (Input.placeholder [] (text "Username"))
+            , text = value
+            }
         ]
+
+
+
+-- old
+-- header
+--     [ class "header" ]
+--     [ h1 [] [ text "Register for Scrambled!" ]
+--     , input
+--         [ class "new-todo"
+--         , placeholder "Your nickname (you use in Diabotical)"
+--         , autofocus True
+--         , value task
+--         , name "nickname"
+--         , onInput UpdateNicknameField
+--         , onEnter RegisterButtonClicked
+--         ]
+--         []
 
 
 onEnter : Msg -> Attribute Msg
@@ -234,14 +308,15 @@ onEnter msg =
         isEnter code =
             if code == 13 then
                 Json.succeed msg
+
             else
                 Json.fail "not ENTER"
     in
-        on "keydown" (Json.andThen isEnter keyCode)
+    on "keydown" (Json.andThen isEnter keyCode)
 
 
 infoFooter : Html msg
 infoFooter =
     footer [ class "info" ]
-        [ p [] [ text "Click the button to be awesome" ]
+        [ p [] [ Html.text "Click the button to be awesome" ]
         ]
