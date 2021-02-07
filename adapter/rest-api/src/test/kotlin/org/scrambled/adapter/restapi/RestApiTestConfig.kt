6a00b1ff035c390afa2296d8c12050971bc1a990
Tim Schraepen <sch3lp@gmail.com>
2021-02-07 17:01:44 +0100

    ✅ @MockBean our Query and CommandExecutors so mockMvc can instantiate our Controllers

--- adapter/rest-api/src/test/kotlin/org/scrambled/adapter/restapi/RestApiTestConfig.kt
+++ adapter/rest-api/src/test/kotlin/org/scrambled/adapter/restapi/RestApiTestConfig.kt
@@ -1,8 +1,18 @@
 package org.scrambled.adapter.restapi
 
+import org.scrambled.infra.cqrs.CommandExecutor
+import org.scrambled.infra.cqrs.QueryExecutor
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import java.lang.Exception
 
 @ComponentScan
 @Configuration
-class RestApiTestConfig
+class RestApiTestConfig {
+
+    @MockBean
+    lateinit var commandExecutor: CommandExecutor
+    @MockBean
+    lateinit var queryExecutor: QueryExecutor
+}
