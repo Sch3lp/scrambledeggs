e82b63ba42f8bfbfc1f7cd69c4f78fc9946a257b
Tim Schraepen <sch3lp@gmail.com>
2021-02-28 17:54:02 +0100

    🔨Epic clean-up. Because we don't depend on spring-boot for most modules, we no longer get the bootJar task which was overriding the regular jar files, so we no longer need the hack. 🎉

--- buildSrc/src/main/kotlin/scrambledeggs.spring-boot-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/scrambledeggs.spring-boot-conventions.gradle.kts
@@ -1,23 +1,9 @@
-import org.gradle.kotlin.dsl.kotlin
-import org.gradle.kotlin.dsl.version
-
 plugins {
     id("scrambledeggs.kotlin-common-conventions")
-    id("scrambledeggs.spring-common-conventions")
-    id("org.springframework.boot") apply false
+    id("scrambledeggs.spring-web-conventions")
+    id("org.springframework.boot")
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-}
-
-tasks.getByName<Jar>("jar") {
-    enabled = true
-}
-
-tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar> {
-    classifier = "boot"
-    mainClassName = "snarf"
 }
