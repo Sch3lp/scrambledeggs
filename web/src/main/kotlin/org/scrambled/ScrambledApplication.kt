2ebe604966ce2960e43a3820eb5005769a0fbb06
Tim Schraepen <sch3lp@gmail.com>
2021-08-24 21:05:06 +0200

    🔨 Making sure calls that don't go to /api are in fact being forwarded to static serving of html

--- web/src/main/kotlin/org/scrambled/ScrambledApplication.kt
+++ web/src/main/kotlin/org/scrambled/ScrambledApplication.kt
@@ -2,7 +2,10 @@ package org.scrambled
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 @EnableScheduling
@@ -13,5 +16,14 @@ class ScrambledApplication {
         fun main(args: Array<String>) {
             runApplication<ScrambledApplication>(*args)
         }
+    }
+}
+
+@Configuration
+class WebApplicationConfig: WebMvcConfigurer {
+    // Paths that do not match the pattern for a static resource (containing a '.'), will be forwarded to root (and will be served the vue frontend)
+    // Since it's a forward, the url will stay the same. This makes it so vue can route to the correct page.
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/**/{spring:[^.]+}").setViewName("forward:/")
     }
 }
