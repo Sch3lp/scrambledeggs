095302dbac92929a3f1442fedaad87f3fd573c9e
Tim Schraepen <sch3lp@gmail.com>
2021-09-22 16:23:01 +0200

    ✅ Moved ChallengeTest to the correct module

--- domain/core/src/test/kotlin/org/scrambled/core/impl/challenges/ChallengeTest.kt
+++ domain/core/src/test/kotlin/org/scrambled/core/impl/challenges/ChallengeTest.kt
@@ -1,9 +1,7 @@
 package org.scrambled.core.impl.challenges
 
-import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 import org.scrambled.domain.core.api.UsefulString
 import org.scrambled.domain.core.api.challenging.GameMode
 import org.scrambled.domain.core.api.exceptions.NotValidException
@@ -15,8 +13,16 @@ class ChallengeTest {
     fun `Cannot create a Challenge to yourself`() {
         val challengerId = UUID.randomUUID()
 
-        assertThatExceptionOfType(NotValidException::class.java)
-            .isThrownBy { Challenge.createChallenge(challengerId, challengerId, UsefulString("comment"), UsefulString("some day"), GameMode.CTF) }
+        Assertions.assertThatExceptionOfType(NotValidException::class.java)
+            .isThrownBy {
+                Challenge.createChallenge(
+                    challengerId,
+                    challengerId,
+                    UsefulString("comment"),
+                    UsefulString("some day"),
+                    GameMode.CTF
+                )
+            }
             .withMessage("You cannot challenge yourself.")
     }
 }
